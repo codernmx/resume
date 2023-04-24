d91868ba9f07a3e61065dd9b8caec97258deaaa2
codernmx <2507128400@qq.com>
2023-04-25 01:20:28 +0800

    feat:tags

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,7 +4,40 @@ const app = getApp<IAppOption>()
 
 Page({
 	data: {
-
+		tags:[
+			{
+				type:'primary',
+				name:'Vue2/Vue3'
+			},
+			{
+				type:'success',
+				name:'React'
+			},
+			{
+				type:'danger',
+				name:'NodeJs'
+			},
+			{
+				type:'warning',
+				name:'小程序'
+			},
+			{
+				type:'primary',
+				name:'Git'
+			},
+			{
+				type:'success',
+				name:'Vite'
+			},
+			{
+				type:'danger',
+				name:'Es6'
+			},
+			{
+				type:'warning',
+				name:'Mysql'
+			}
+		]
 	},
 
 	onLoad() {
