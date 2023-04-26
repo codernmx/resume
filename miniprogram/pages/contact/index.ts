1eca6790127b13ffe4cb2b42be1838f81e810c05
codernmx <2507128400@qq.com>
2023-04-27 01:02:48 +0800

    fix:接口返回数据

--- miniprogram/pages/contact/index.ts
+++ miniprogram/pages/contact/index.ts
@@ -5,14 +5,14 @@ Page({
          * 页面的初始数据
          */
         data: {
-
+                userInfo: {}
         },
 
         /**
          * 生命周期函数--监听页面加载
          */
         onLoad() {
-
+                this.setData({ userInfo: getApp().globalData.userInfo })
         },
         call() {
                 wx.makePhoneCall({
@@ -73,5 +73,6 @@ Page({
          */
         onShareAppMessage() {
 
-        }
+        },
+        onShareTimeline() { }
 })
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,45 +4,65 @@ const app = getApp<IAppOption>()
 
 Page({
 	data: {
-		tags:[
+		tags: [
 			{
-				type:'primary',
-				name:'Vue2/Vue3'
+				type: 'primary',
+				name: 'Vue2/Vue3'
 			},
 			{
-				type:'success',
-				name:'React'
+				type: 'success',
+				name: 'React'
 			},
 			{
-				type:'danger',
-				name:'NodeJs'
+				type: 'danger',
+				name: 'NodeJs'
 			},
 			{
-				type:'warning',
-				name:'小程序'
+				type: 'warning',
+				name: '小程序'
 			},
 			{
-				type:'primary',
-				name:'Git'
+				type: 'primary',
+				name: 'Git'
 			},
 			{
-				type:'success',
-				name:'Vite'
+				type: 'success',
+				name: 'Vite'
 			},
 			{
-				type:'danger',
-				name:'Es6'
+				type: 'danger',
+				name: 'Es6'
 			},
 			{
-				type:'warning',
-				name:'Mysql'
+				type: 'warning',
+				name: 'Mysql'
 			}
 		]
 	},
 
 	onLoad() {
 		// @ts-ignore
+		wx.showLoading({
+			title: '加载中',
+			mask: true
+		})
+		wx.request({
+			url: 'https://bt.nmxgzs.cn/mini/program/resume/get/userInfo', //仅为示例，并非真实的接口地址
+			method: 'POST',
+			data: {},
+			header: {
+				'content-type': 'application/json' // 默认值
+			},
+			success: (res: any) => {
+				console.log(res.data.data)
+				getApp().globalData.userInfo = res.data.data
+				this.setData({ userInfo: res.data.data })
+				wx.hideLoading()
+			}
+		})
 
 	},
+	onShareAppMessage() { },
+	onShareTimeline() { }
 
 })
